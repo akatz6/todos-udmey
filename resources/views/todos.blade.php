badb1a91406b01ff76a1c39c05ada54c4e30f411
aaronmk2 <aaronmk2@yahoo.com>
2017-12-03 21:31:17 -0800

    basid todo list laravel

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+        <form action="/create/todo" method="post">
+            {{csrf_field()}}
+            <input type="text" class="form-control input-lg" name="todo" placeholder="Create a new todo">
+        </form>
+        </div>
+    </div>
+    <hr>
+    @foreach($todos as $todo)
+        {{$todo->todo}} <a href="{{route('todo.delete', ['id' => $todo->id])}}" class="btn btn-danger">X</a>
+         <a href="{{route('todo.update', ['id' => $todo->id])}}" class="btn btn-info btn-xs">update</a>
+         @if(!$todo->completed)
+            <a href="{{route('todo.completed', ['id' => $todo->id])}}" class="btn btn-success btn-xs">success</a>
+         @endif
+       <hr>
+    @endforeach
+@stop
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+    
+         <a href="/todos">Visit my list</a>
+   
+@stop
